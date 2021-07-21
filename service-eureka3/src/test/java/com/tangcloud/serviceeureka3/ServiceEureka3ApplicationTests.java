1fc7bc1135d493ad781e1b5c41bbbdbb7620e74c
TIM <tanghaifeng93@gmail.com>
2021-07-21 11:08:41 +0800

    eureka 集群配置

---
+++ service-eureka3/src/test/java/com/tangcloud/serviceeureka3/ServiceEureka3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangcloud.serviceeureka3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEureka3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
